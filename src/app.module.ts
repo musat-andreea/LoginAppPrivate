12d2a87216b3fad2689932ed0f153d47eef1f7d3
Andreea Musat <andree98Black@yahoo.com>
2023-11-25 20:41:53 +0200

    feat: added weather and sign up, login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+const entities = [];
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: entities,
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,27 @@
+import * as bcrypt from 'bcryptjs';
+import { BaseEntity, BeforeInsert, Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    @UpdateDateColumn()
+    updateAt: Date;
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+
+    async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+}
